fb5c1b862158988f208e203d07bd58beccae960f
MatthiasLHK <matthiasli1998@hotmail.com>
2021-03-20 11:19:12 +0800

    master

--- app/main.py
+++ app/main.py
@@ -4,5 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    print("Hello in console")
+    cookie = request.args.get('c')
+    print(cookie)
     return "<h1>Welcome to Geeks for Geeks</h>"
